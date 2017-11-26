94a180c510697b241d793ab63857b9b3bff45fde
Nhong Anuchit Prasertsang <anuchitp@thoughtworks.com>
2017-11-26 14:59:39 +0700

    refactor play to struct

--- play/play.go
+++ play/play.go
@@ -3,11 +3,22 @@ package play
 import (
 	"github.com/AnuchitPrasertsang/roshambo/computer"
 	"github.com/AnuchitPrasertsang/roshambo/decide"
+	"github.com/AnuchitPrasertsang/roshambo/score"
 )
 
-func Play(user string) (string, string, string) {
+type Play struct {
+	User     score.Scorer
+	Computer score.Scorer
+}
+
+func New(user, computer score.Scorer) *Play {
+	return &Play{user, computer}
+}
+
+func (p *Play) Play(user string) (string, string, string) {
 	com := computer.Choice()
 	result := play(user, com)
+	score.Count(p.User, result, p.Computer)
 	return user, com, result
 }
 
