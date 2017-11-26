5c90e9c9b041cba6a60ec5fb9b979e73514b1a9e
Nhong Anuchit Prasertsang <anuchitp@thoughtworks.com>
2017-11-26 14:38:04 +0700

    count score

--- score/score.go
+++ score/score.go
@@ -1,5 +1,20 @@
 package score
 
-func Count(resultUser string) {
+import "github.com/AnuchitPrasertsang/roshambo/decide"
 
+func Count(user Scorer, resultUser string, computer Scorer) {
+	if resultUser == decide.WIN {
+		user.IncWin()
+		computer.IncLoss()
+		return
+	}
+
+	if resultUser == decide.LOSS {
+		user.IncLoss()
+		computer.IncWin()
+		return
+	}
+
+	user.IncTie()
+	computer.IncTie()
 }
---
+++ score/score_test.go
@@ -0,0 +1,80 @@
+package score
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/AnuchitPrasertsang/roshambo/decide"
+)
+
+func TestIncreaseWinScore(t *testing.T) {
+	userScore := &Score{
+		Win:  1,
+		Loss: 0,
+		Tie:  0,
+	}
+	comScore := &Score{
+		Win:  0,
+		Loss: 1,
+		Tie:  0,
+	}
+	user := &Score{}
+	computer := &Score{}
+
+	Count(user, decide.WIN, computer)
+
+	if !reflect.DeepEqual(user, userScore) {
+		t.Errorf("expect % #v \n but got % #v", userScore, user)
+	}
+	if !reflect.DeepEqual(computer, comScore) {
+		t.Errorf("expect % #v \n but got % #v", comScore, computer)
+	}
+}
+
+func TestIncreaseLossScore(t *testing.T) {
+	userScore := &Score{
+		Win:  0,
+		Loss: 1,
+		Tie:  0,
+	}
+	comScore := &Score{
+		Win:  1,
+		Loss: 0,
+		Tie:  0,
+	}
+	user := &Score{}
+	computer := &Score{}
+
+	Count(user, decide.LOSS, computer)
+
+	if !reflect.DeepEqual(user, userScore) {
+		t.Errorf("expect % #v \n but got % #v", userScore, user)
+	}
+	if !reflect.DeepEqual(computer, comScore) {
+		t.Errorf("expect % #v \n but got % #v", comScore, computer)
+	}
+}
+
+func TestIncreaseTieScore(t *testing.T) {
+	userScore := &Score{
+		Win:  0,
+		Loss: 0,
+		Tie:  1,
+	}
+	comScore := &Score{
+		Win:  0,
+		Loss: 0,
+		Tie:  1,
+	}
+	user := &Score{}
+	computer := &Score{}
+
+	Count(user, decide.TIE, computer)
+
+	if !reflect.DeepEqual(user, userScore) {
+		t.Errorf("expect % #v \n but got % #v", userScore, user)
+	}
+	if !reflect.DeepEqual(computer, comScore) {
+		t.Errorf("expect % #v \n but got % #v", comScore, computer)
+	}
+}
