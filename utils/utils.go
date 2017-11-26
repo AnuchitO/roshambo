825b22c9acea05c7be9e73f8c55528be369ffb66
Nhong Anuchit Prasertsang <anuchitp@thoughtworks.com>
2017-11-26 11:03:22 +0700

    expose function mapuserKey

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,7 @@ package utils
 
 import "github.com/AnuchitPrasertsang/roshambo/decide"
 
-func mapUserKey(key string) string {
+func MapUserKey(key string) string {
 	m := map[string]string{
 		"w": decide.Scissors,
 		"b": decide.Rock,
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestUserKeyMapping(t *testing.T) {
-	if mapUserKey("w") != decide.Scissors {
-		t.Error("expect Scissors but got", mapUserKey("w"))
+	if MapUserKey("w") != decide.Scissors {
+		t.Error("expect Scissors but got", MapUserKey("w"))
 	}
-	if mapUserKey("b") != decide.Rock {
-		t.Error("expect Rock but got", mapUserKey("b"))
+	if MapUserKey("b") != decide.Rock {
+		t.Error("expect Rock but got", MapUserKey("b"))
 	}
-	if mapUserKey("p") != decide.Paper {
-		t.Error("expect Paper but got", mapUserKey("p"))
+	if MapUserKey("p") != decide.Paper {
+		t.Error("expect Paper but got", MapUserKey("p"))
 	}
 }
